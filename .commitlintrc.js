73fae941b8bc6d8cecc17a20f7ec0a2e0e282f03
see <upcwangying@126.com>
2022-08-17 09:21:23 +0800

    fix: bugfixes

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -14,7 +14,7 @@ module.exports = {
       [
         'docs',
         ...swPackages,
-      ].map(name => `$${name}`)
+      ].map(name => `$${name}`).concat('release')
     ],
     'subject-case': [0, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
   }
