aa621517c79ee2fff61f750a9cf109bc2cda0269
see <upcwangying@126.com>
2022-08-22 11:02:51 +0800

    fix: Add `jsx` support

--- packages/components/config/utils.js
+++ packages/components/config/utils.js
@@ -2,9 +2,9 @@ const glob = require('glob')
 
 function getEntries() {
   let map = {}
-  const entryFiles = glob.sync('./src/**/*.js')
+  const entryFiles = glob.sync('./src/**/*.js?(x)')
   entryFiles.forEach((filepath) => {
-    let fileDir = /.\/src\/(.*?)\.js/.exec(filepath)
+    let fileDir = /.\/src\/(.*?)\.jsx?/.exec(filepath)
     map[fileDir[1]] = filepath
   })
   return map
