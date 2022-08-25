f5865d5f0fa1bc516b20d4bc4ea59441eac1cdad
see <upcwangying@126.com>
2022-08-25 22:39:42 +0800

    fix: ABMLoading

--- packages/components/src/ABMLoading/index.js
+++ packages/components/src/ABMLoading/index.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
-import styles from './index.module.less'
+import './index.less'
 
 export class ABMLoading extends Component {
   render() {
     const { platformName } = this.props
     return (
-      <div className={styles.abmPreScreenLoading}>
-        <div className={styles.abmPreScreenLoadingCenter}>
-          <div className={styles.abmPreScreenLoadingCenterAbsolute}>
-            <div className={{ ...styles.abmPreScreenObject, ...styles.abmPreScreenObject_four }} />
-            <div className={{ ...styles.abmPreScreenObject, ...styles.abmPreScreenObject_three }} />
-            <div className={{ ...styles.abmPreScreenObject, ...styles.abmPreScreenObject_two }} />
-            <div className={{ ...styles.abmPreScreenObject, ...styles.abmPreScreenObject_one }} />
+      <div className={'abm-pre-screen-loading'}>
+        <div className={'abm-pre-screen-loading-center'}>
+          <div className={'abm-pre-screen-loading-center-absolute'}>
+            <div className={'abm-pre-screen-object abm-pre-screen-object_four'} />
+            <div className={'abm-pre-screen-object abm-pre-screen-object_three'} />
+            <div className={'abm-pre-screen-object abm-pre-screen-object_two'} />
+            <div className={'abm-pre-screen-object abm-pre-screen-object_one'} />
           </div>
-          <h1 className={styles.abmPreScreenText}>
+          <h1 className={'abm-pre-screen-text'}>
             <span>{platformName}</span>
           </h1>
-          <h1 className={styles.abmPreScreenText}>
+          <h1 className={'abm-pre-screen-text'}>
             <span>{platformName}</span>
           </h1>
         </div>
