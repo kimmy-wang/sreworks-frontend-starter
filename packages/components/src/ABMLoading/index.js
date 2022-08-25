32b5c350bced4764901ce1ddf49e02aa94e8dfa3
see <upcwangying@126.com>
2022-08-25 22:58:28 +0800

    fix: ABMLoading

--- packages/components/src/ABMLoading/index.js
+++ packages/components/src/ABMLoading/index.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react'
+import classNames from 'classnames'
 import styles from './index.module.less'
 
 export class ABMLoading extends Component {
   render() {
     const { platformName } = this.props
     return (
-      <div className={styles.abmPreScreenLoading}>
-        <div className={styles.abmPreScreenLoadingCenter}>
-          <div className={styles.abmPreScreenLoadingCenterAbsolute}>
-            <div className={{ ...styles.abmPreScreenObject, ...styles.abmPreScreenObject_four }} />
-            <div className={{ ...styles.abmPreScreenObject, ...styles.abmPreScreenObject_three }} />
-            <div className={{ ...styles.abmPreScreenObject, ...styles.abmPreScreenObject_two }} />
-            <div className={{ ...styles.abmPreScreenObject, ...styles.abmPreScreenObject_one }} />
+      <div className={styles.loading}>
+        <div className={styles.loadingCenter}>
+          <div className={styles.loadingCenterAbsolute}>
+            <div className={classNames(styles.loadingObject, styles.loadingObject_four)} />
+            <div className={classNames(styles.loadingObject, styles.loadingObject_three)} />
+            <div className={classNames(styles.loadingObject, styles.loadingObject_two)} />
+            <div className={classNames(styles.loadingObject, styles.loadingObject_one)} />
           </div>
-          <h1 className={styles.abmPreScreenText}>
+          <h1 className={styles.loadingText}>
             <span>{platformName}</span>
           </h1>
-          <h1 className={styles.abmPreScreenText}>
+          <h1 className={styles.loadingText}>
             <span>{platformName}</span>
           </h1>
         </div>
