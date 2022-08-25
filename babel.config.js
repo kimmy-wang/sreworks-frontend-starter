f100c6af28c367dcef8fcdf36d4b717111e3cf48
see <upcwangying@126.com>
2022-08-25 08:59:18 +0800

    build: add app

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,8 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          useBuiltIns: 'entry',
-          corejs: '3.22',
-        },
-      ],
-    ],
     babelrcRoots: [
-      '.',
+      'app/*',
       'packages/*', // 将子程序包都作为工作目录
     ],
   }
